1bbd5385678b56c9f0e20136a182afc41411ba60
Ted Tyree <tedtyree@gmail.com>
2026-02-05 21:58:02 -0500

    debug login issue

--- websites_example/hostsite/require/website_engine.js
+++ websites_example/hostsite/require/website_engine.js
@@ -146,8 +146,10 @@ class webEngine {
                     await cms.SessionLogin(username,password,cms.siteId);
                     //if (cms.debugMode>3) {
                         // DEBUGGER WARNING! user record contains sensitive information
-                        console.log("DEBUGGER: user record=[" + stringify.JSON(cms.user) + "]\n");
-                        console.log("cms.debugMode=" + cms.debugMode);
+                        //console.log("DEBUGGER: user record=[" + stringify.JSON(cms.user) + "]\n");
+                        //console.log("cms.debugMode=" + cms.debugMode);
+                        cms.logMessage(3,"DEBUGGER: user record=[" + stringify.JSON(cms.user) + "]\n");
+                        cms.logMessage(3,"cms.debugMode=" + cms.debugMode);
                     //}
                     if (cms.user.userKey < 0) {
                         this.errorMessage = 'login not successful';
